0ceec72bab59a42a96cf2a61a9ef2555de0fa318
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2024-04-26 16:53:06 -0300

    ...

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+// Config JSON response
+
+app.use(express.json())
+
+// solve CORS
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
+
+// public folder for images
+app.use(express.static('public'))
+
+// Routes
+
+app.listen(5000)
