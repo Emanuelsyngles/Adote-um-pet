a70fcf67d6cc343b398b79bd75a37af244c7dbe4
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2024-04-29 16:44:10 -0300

    Update UserController.js

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,13 +1,34 @@
 const User = require('../models/User')
 
 module.exports = class UserController {
-    static async register(req, res) {
-        
-        const name = req.body.name
-        const email = req.body.email
-        const phone = req.body.phone
-        const password = req.body.password
-        const confirmpassword = req.body.confirmpassword
+    static async register(req, res) {     
+        const {name, email, phone, password, confirmpassword} = req.body
 
+        // validações
+
+        if(!name) {
+            res.status(422).json({message: 'O nome é obrigatório!'})
+            return
+        }
+        if(!email) {
+            res.status(422).json({message: 'O e-mail é obrigatório!'})
+            return
+        }
+        if(!phone) {
+            res.status(422).json({message: 'O telefone é obrigatório!'})
+            return
+        }
+        if(!password) {
+            res.status(422).json({message: 'a senha é obrigatória!'})
+            return
+        }
+        if(!confirmpassword) {
+            res.status(422).json({message: 'a confirmação da senha é obrigatória!'})
+            return
+        }
+        if(password !== confirmpassword) {
+            res.status(422).json({message: 'As senhas não conhecidem, por favor tenta novamente.'})
+            return
+        }
     }
 }
