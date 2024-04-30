7443d81aaace93e850f0d78be687b82bd86a3a0d
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2024-04-30 15:30:57 -0300

    Update conn.js

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
+const { options } = require('../routes/UserRoutes')
 
 async function main() {
-    await mongoose.connect('mongodb://localhost:27017/getapet')
-    console.log('Conectou ao Mongoose!') 
+  await mongoose.connect('mongodb://localhost:27017/getapetref', options)
+  console.log('Conectou com Mongoose!')
 }
 
 main().catch((err) => console.log(err))
