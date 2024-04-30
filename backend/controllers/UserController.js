5e47dc41c2efed18ce57bad8945be92d531c498b
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2024-04-30 14:22:45 -0300

    Update UserController.js

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,5 +1,7 @@
 const User = require('../models/User')
 
+const bcrypt = require('bcrypt')
+
 module.exports = class UserController {
     static async register(req, res) {     
         const {name, email, phone, password, confirmpassword} = req.body
@@ -41,5 +43,28 @@ module.exports = class UserController {
             })
             return
         }
+
+        // create a password
+        const salt = await bcrypt.genSalt(12)
+        const passwordHash = await bcrypt.hash(password, salt)
+
+        // create a user
+        const user = new User( {
+            name,
+            email,
+            phone,
+            password: passwordHash,
+        })
+
+        try {
+           const newUser = await user.save()
+           res.status(201).json({
+            message: 'Usuário criado!',
+            newUser,
+           })
+        } catch (error) {
+            res.status(500).json({message: error})
+        }
+
     }
 }
